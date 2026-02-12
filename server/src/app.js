962154895848fd1cd3b90dacc9c5cea70daffa9f
HIRUMI SENEVIRATHNA <hirumisenevirathna24@gmail.com>
2026-02-12 22:25:12 +0530

    Do Some Updates

--- server/src/app.js
+++ server/src/app.js
@@ -2,12 +2,18 @@ const express = require("express");
 const cors = require("cors");
 
 const authRoutes = require("./routes/auth.routes");
-
 const issueRoutes = require("./routes/issue.routes");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "https://trustworthy-courage-production.up.railway.app",
+    "http://localhost:5173"
+  ],
+  credentials: true
+}));
+
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
